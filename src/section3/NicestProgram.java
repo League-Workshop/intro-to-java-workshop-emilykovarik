80b01a45c9caa70349305c3cf623607c7b0e27df
rtgamer8 <league@WTS21.attlocal.net>
2018-06-20 11:59:45 -0700

    Candy Crush, Greeter, Hello World, Mad Libs,Nicest Program, Stephen Hawking, World Domination, Speak and Spell(IP)

---
+++ src/section3/NicestProgram.java
@@ -0,0 +1,14 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class NicestProgram {
+	public static void main(String[] args) {
+		
+JOptionPane.showMessageDialog(null, "I like your shoes");
+JOptionPane.showMessageDialog(null, "You are a good programer");
+
+System.out.println("I like koalas");
+JOptionPane.showInputDialog("How are you?");
+}
+}
--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,11 +9,14 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("e.g. spell mandlebrot");
 		// 2. Catch the user's answer in a String
-
+String answer=JOptionPane.showInputDialog(Can you spell mandlebrot?);
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if(answer.equals("yes")) {
+	
+}
+}
 		// 4. Otherwise say "wrong"
 
 		// 5. repeat the process for other words
