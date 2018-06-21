dcf0590361a8441474ff929cb60c6ecadad6de38
emilykovarik <league@WTS21.attlocal.net>
2018-06-21 12:04:08 -0700

    Day 4

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -11,17 +11,15 @@ public class SpeakAndSpell {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
 speak("e.g. spell mandlebrot");
 		// 2. Catch the user's answer in a String
-String answer=JOptionPane.showInputDialog(Can you spell mandlebrot?);
+//String answer=JOptionPane.showInputDialog(Can you spell mandlebrot?);
 		// 3. If the user spelled the word correctly, speak "correct"
-if(answer.equals("yes")) {
-	
-}
+
 }
 		// 4. Otherwise say "wrong"
 
 		// 5. repeat the process for other words
 		
-	}
+	
 
 	static void speak(String words) {
 		try {
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,30 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score=0;
 		// 2.  Ask the user a question 
-		
+	String answer=	JOptionPane.showInputDialog(null, "What Disney movie does Emma Watson play as Belle in?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		String value= "Beauty and the Beast"; 
 		// 4.  if the user's answer is correct
-		
+		if(answer.equals(value)) {score++;
 		// -- add one to their score 
-		
+		}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		String character=JOptionPane.showInputDialog(null,"What Disney princess loses a glass slipper?");
+		if(character.equals("Cinderella")) {score++;
+		}
 		
+		String tale=JOptionPane.showInputDialog(null, "In what tale does a girl carry a basket?");
+		if(tale.equals("Little Red Riding Hood")) {score++;
+		}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		System.out.println(score++);
 	}
 }
